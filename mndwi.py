10821ab2427a6ebdc447a32f9f06e4c8e65582b5
DangerSandwich <39280800+DangerSandwich@users.noreply.github.com>
2018-05-14 16:33:27 -0700

    Update and
    MNDI (Modified Normalized Water Index) used for

--- mndwi.py
+++ mndwi.py
@@ -3,66 +3,66 @@ import numpy as np
 import os
 import subprocess
 
-#calculate the ndvi and return the index
-def calculateNDVI(tile):
+#calculate the mndwi (modified normalized water index by Xu 2006) and return the index
+def calculateMNDWI(tile):
 
     count = 0
-    #Pick up the files of B04 and B08
-    band4_dir = resample_dir + tile + '/' + 'B04/'
-    band8_dir = resample_dir + tile + '/' + 'B08/'
-    band4_list = []
-    band8_list = []
-    for (dirpath, dirnames, filenames) in os.walk(band4_dir):
-        band4_list.extend(filenames)
-    for (dirpath, dirnames, filenames) in os.walk(band8_dir):
-        band8_list.extend(filenames)
-    #pick up the B04 and B08 and flatten the matrix to array
-    band4_matrix = []
-    band8_matrix = []
+    #Pick up the files of B03 and B11
+    band03_dir = resample_dir + tile + '/' + 'B03/'
+    band11_dir = resample_dir + tile + '/' + 'B11/'
+    band03_list = []
+    band11_list = []
+    for (dirpath, dirnames, filenames) in os.walk(band03_dir):
+        band03_list.extend(filenames)
+    for (dirpath, dirnames, filenames) in os.walk(band11_dir):
+        band11_list.extend(filenames)
+    #pick up the B03 and B11 and flatten the matrix to array
+    band03_matrix = []
+    band11_matrix = []
     #sort two file list
-    band4_list.sort()
-    band8_list.sort()
-    for b4 in band4_list:
+    band03_list.sort()
+    band11_list.sort()
+    for b03 in band03_list:
         # file_dir = resample_dir + zone + '/' + tile + '/' + band
-        file_dir = band4_dir + b4
+        file_dir = band03_dir + b03
         raster = gdal.Open(file_dir)
         banddataraster = raster.GetRasterBand(1)
         dataraster = banddataraster.ReadAsArray().astype(np.float)
         shape = dataraster.shape
-        band4_matrix.append(dataraster.flatten())
+        band03_matrix.append(dataraster.flatten())
 
-    for b8 in band8_list:
+    for b03 in band11_list:
         # file_dir = resample_dir + zone + '/' + tile + '/' + band
-        file_dir = band8_dir + b8
+        file_dir = band11_dir + b11
         raster = gdal.Open(file_dir)
         banddataraster = raster.GetRasterBand(1)
         dataraster = banddataraster.ReadAsArray().astype(np.float)
-        band8_matrix.append(dataraster.flatten())
-    # np.savetxt('test.txt', np.array(band4_list), delimiter=',')
-    # np.savetxt('test.txt', np.array(band8_list), delimiter = ',')
+        band11_matrix.append(dataraster.flatten())
+    # np.savetxt('test.txt', np.array(band03_list), delimiter=',')
+    # np.savetxt('test.txt', np.array(band11_list), delimiter = ',')
     #transform list back to np-array(matrix)
     geoTransform = raster.GetGeoTransform()
     geoProjection = raster.GetProjection()
     shape = dataraster.shape
-    band4_matrix = np.array(band4_matrix)
-    band8_matrix = np.array(band8_matrix)
-    diff = band8_matrix - band4_matrix
-    summ = band4_matrix + band8_matrix
-    ndvi = diff / summ
-    ndvi[np.where(summ == 0)] = -1
-    ndvi = np.where(ndvi == 1, -1, ndvi)
-    ndvi_pickup = np.amax(ndvi, axis = 0)
-    index_max = np.argmax(ndvi, axis = 0)
-    ndvi_matrix = ndvi_pickup.reshape(shape)
-    np.savetxt("test.txt", ndvi_matrix, delimiter = ',')
+    band03_matrix = np.array(band03_matrix)
+    band11_matrix = np.array(band11_matrix)
+    diff = band03_matrix - band11_matrix
+    summ = band03_matrix + band11_matrix
+    mndwi = diff / summ
+    mndwi[np.where(summ == 0)] = -1
+    mndwi = np.where(mndwi == 1, -1, ndvi)
+    mndwi_pickup = np.amax(mndwi, axis = 0)
+    index_max = np.argmax(mndwi, axis = 0)
+    ndvi_matrix = mndwi_pickup.reshape(shape)
+    np.savetxt("test.txt", mndwi_matrix, delimiter = ',')
     #call the create_tiff function to transform the output into a tif
-    tif_dir = output_dir + 'NDVI/'
-    tif_name = tif_dir + tile + '_NDVI.tif'
+    tif_dir = output_dir + 'MNDWI/'
+    tif_name = tif_dir + tile + '_MNDWI.tif'
     if os.path.isfile(tif_name):
         return index_max
     if not os.path.exists(tif_dir):
         os.makedirs(tif_dir)
-    create_tif(tif_name, ndvi_matrix, geoTransform, geoProjection)
+    create_tif(tif_name, mndwi_matrix, geoTransform, geoProjection)
     return index_max
 
 #define the function to create the tif file
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     bands = ['B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B11', 'B12', 'B8A']
     count = 0
     for tile in tile_list_test:
-        print "calculate the NDVI of tile", tile
+        print "calculate the MNDWI of tile", tile
         index = calculateNDVI(tile)
         for band in bands:
             print "pick up", band
